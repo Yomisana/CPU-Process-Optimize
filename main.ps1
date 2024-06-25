23fcdf3f42f5cea4f87bb963629229c0da2d4519
Yomisana <yomisana3236@gmail.com>
2024-06-26 03:27:16 +0800

    add option & release v1

--- main.ps1
+++ main.ps1
@@ -1,5 +1,12 @@
-﻿# 設定要監測的進程名稱
+﻿param (
+    # 是否開啟顯示當前監控所有進程資訊(預設開啟)
+    [switch]$ShowProcessInfo = $true
+    # [switch]$ShowProcessInfo = $false
+)
+
+# 設定要監測的進程名稱(預設:針對 edge 的 webview2)
 $processName = "msedgewebview2"
+# $processName = "chrome"
 
 # 設定檢查間隔時間（秒）
 $interval = 5
@@ -9,6 +16,21 @@ $totalCores = [Environment]::ProcessorCount
 
 # 用於儲存已檢測到的進程 ID 和其使用的核心
 $existingProcesses = @{}
+$coreUsageCount = @{}
+
+# 隨機選擇一個未被分配的核心，如果所有核心都被分配過，則選擇當前被使用次數最少的核心
+function Get-Next-Core {
+    $usedCores = $existingProcesses.Values | ForEach-Object { $_.UsedCores } | Select-Object -Unique
+    $unusedCores = @(0..($totalCores-1)) | Where-Object { $_ -notin $usedCores }
+
+    if ($unusedCores.Count -gt 0) {
+        return $unusedCores | Get-Random
+    } else {
+        $minUsage = $coreUsageCount.Values | Measure-Object -Minimum | Select-Object -ExpandProperty Minimum
+        $leastUsedCores = $coreUsageCount.Keys | Where-Object { $coreUsageCount[$_] -eq $minUsage }
+        return $leastUsedCores | Get-Random
+    }
+}
 
 # 持續監測進程
 while ($true) {
@@ -24,6 +46,11 @@ while ($true) {
     foreach ($processId in $existingProcessIds) {
         if (-not ($currentProcessIds -contains $processId)) {
             # 移除已經關閉的進程
+            $coreToRemove = $existingProcesses[$processId].UsedCores
+            foreach ($core in $coreToRemove) {
+                $coreUsageCount[$core]--
+            }
+
             $existingProcesses.Remove($processId) | Out-Null
             Write-Output "進程 ID $processId 已關閉，從列表中移除" (Get-Date)
         }
@@ -46,16 +73,20 @@ while ($true) {
                     $coreIndex++
                 }
 
-                # 從可用核心中隨機選擇一個核心
-                $random = New-Object System.Random
-                $randomCore = $random.Next(0, $totalCores)
+                # 獲取下一個核心進行分配
+                $nextCore = Get-Next-Core
 
                 # 設置進程只使用選定的核心
-                $newAffinity = [int][math]::Pow(2, $randomCore)
+                $newAffinity = [int][math]::Pow(2, $nextCore)
                 $process.ProcessorAffinity = [IntPtr]$newAffinity
 
                 # 更新使用的核心信息
-                $usedCores = @($randomCore)
+                if (-not $coreUsageCount.ContainsKey($nextCore)) {
+                    $coreUsageCount[$nextCore] = 0
+                }
+                $coreUsageCount[$nextCore]++
+
+                $usedCores = @($nextCore)
 
                 # 將新進程加入到已檢測到的進程字典中
                 $existingProcesses[$process.Id] = @{
@@ -64,16 +95,20 @@ while ($true) {
                 }
 
                 # 顯示新進程信息
-                Write-Output "偵測到新的進程 ID $($process.Id)，名稱為 $($process.ProcessName)，將其設置為使用單一核心:$randomCore" (Get-Date)
+                Write-Output "偵測到新的進程 ID $($process.Id)，名稱為 $($process.ProcessName)，將其設置為使用單一核心:$nextCore" (Get-Date)
             }
         }
     }
 
-    # 顯示目前已檢測到的所有進程及其信息，並添加當前的日期時間
-    Write-Output "`n目前已檢測到的進程信息：" (Get-Date)
-    foreach ($processId in $existingProcesses.Keys) {
-        $processInfo = $existingProcesses[$processId]
-        Write-Output "進程 ID: $processId，名稱: $($processInfo.ProcessName)，使用核心: $($processInfo.UsedCores -join ', ')"
+    # 如果參數設為顯示，則顯示目前已檢測到的所有進程及其信息，並添加當前的日期時間
+    if ($ShowProcessInfo) {
+        Write-Output "`n目前已檢測到的進程信息：" (Get-Date)
+        $totalProcesses = $existingProcesses.Count
+        Write-Output "目前總共有 $totalProcesses 個進程："
+        foreach ($processId in $existingProcesses.Keys) {
+            $processInfo = $existingProcesses[$processId]
+            Write-Output "進程 ID: $processId，名稱: $($processInfo.ProcessName)，使用核心: $($processInfo.UsedCores -join ', ')"
+        }
     }
 
     # 等待指定的間隔時間
